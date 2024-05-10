72e93b767ff6878a5e3da0996d205afb7bfce82f
xyz148 <zxy30730@gmail.com>
2024-05-10 07:29:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,166 +24,128 @@ bot = Client(
     api_hash=os.environ.get("API_HASH")
 )
 
-logger = logging.getLogger()
-# thumb = os.environ.get("THUMB")
-# if thumb.startswith("http://") or thumb.startswith("https://"):
-#     getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
-#     thumb = "thumb.jpg"
-
-@bot.on_message(filters.command(["start"]))
-async def start(bot, update):
-       await update.reply_text("Hi i am **Careerwill Downloader**.\n\n"
-                              "**NOW:-** "
-                                       
-                                       "first join here - @BOT_UPDATES_by_RAJ for command ..\n\n"
-                                     "Bot made by **@Sccwaleyarr ...❤️‍🔥raj🥀**" )
-
-ACCOUNT_ID = "6206459123001"
-BCOV_POLICY = "BCpkADawqM1474MvKwYlMRZNBPoqkJY-UWm7zE1U769d5r5kqTjG0v8L-THXuVZtdIQJpfMPB37L_VJQxTKeNeLO2Eac_yMywEgyV9GjFDQ2LTiT4FEiHhKAUvdbx9ku6fGnQKSMB8J5uIDd"
-bc_url = (
-    f"https://edge.api.brightcove.com/playback/v1/accounts/{ACCOUNT_ID}/videos"
-)
-bc_hdr = {"BCOV-POLICY": BCOV_POLICY}
-
-url="https://elearn.crwilladmin.com/api/v1/"
-
-info= {
- "deviceType":"android",
-    "password":"",
-    "deviceModel":"Asus ASUS_X00TD",
-    "deviceVersion":"Pie(Android 9.0)",
-    "email":"",
-}
-
-@bot.on_message(filters.command("fuckcw"))
-async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text(
-        "Send ID & Password in this manner otherwise bot will not respond.\n\nSend like this:-  ID*Password"
-    )
-    input1: Message = await bot.listen(editable.chat.id)
-    raw_text = input1.text
-    await input1.delete(True)
+async def fetch_data(session, url, headers=None):
+    async with session.get(url, headers=headers) as response:
+        return await response.json()
+    
+def decrypt_link(link):
     try:
-        if "*" in raw_text:
-            em = raw_text.split("*")[0]
-            ps = raw_text.split("*")[1]
-            payload={ "deviceType": "android", "password": ps, "deviceIMEI": "", "deviceModel": "XiaoMi M2007J17C", "deviceVersion": "upper than 31", "email": em, "deviceToken": "eX1ixOOrRZuc7cUygJpdDl:APA91bGBGSGDyupdzriHH0Hi0yvOH6ID2jYFsxz0Y_RdFcAUUGsSJL-mNxKARjBC9zoyjQT-C0OD48It06MyahSYGwccvQ4TeQDKkSQ50jJAYHXKz3b_WdfQPBWlsn2GMOxFPSvxNyYw" }
-            r = requests.post('https://elearn.crwilladmin.com/api/v5/login-other', headers=hdr, json=payload).json()
-            rdata = r.get("data", {})
-            token = rdata.get("token", '')
-            await editable.edit(f"Login Successful")
-        else:
-            token = raw_text
+        decoded_link = base64.b64decode(link)
+        key = b'638udh3829162018'
+        iv = b'fedcba9876543210'
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        decrypted_link = unpad(cipher.decrypt(decoded_link), AES.block_size).decode('utf-8')
+        return decrypted_link
+    except ValueError as ve:
+        print(f"Padding error while decrypting link: {ve}")
     except Exception as e:
-        await editable.edit(f"An error occurred during login: {e}")
-        return
+        print(f"Error decrypting link: {e}")
 
+async def handle_appxv2_logic(bot: bot, m: Message, api_endpoint: str):
+    editable = await m.reply_text("Send **ID & Password** in this manner otherwise bot will not respond.\n\nSend like this:-  **ID*Password**")
+    input1 = await bot.listen(editable.chat.id)
     headers = {
-    'token': token,
-    'usertype':'2',
-    'appver':'86',
-    'apptype':'android',
-    'accept-encoding':'gzip',
-    'user-agent':'okhttp/5.0.0-alpha.2'}
-
-    url1 = requests.get('https://elearn.crwilladmin.com/api/v5/my-batch', headers=headers)
-    keydata = json.loads(url1.text)
-    print(keydata)
-    data = keydata.get("data", {})
-    bdata = data.get("batchData", [])
-    print(bdata)
-
-    if not bdata:
-        await editable.edit("You don't have any batches available.")
-        return
-
-    cool = ""
-    FFF = "BATCH-ID  -  BATCH NAME"
-    for item in bdata:
-        id = item.get("id")
-        btchnam = item.get("batchName")
-        print(btchnam)
-        insname = item.get("instructorName")
-        aa = f"{id} - {btchnam}-->{insname}\n\n"
-        if len(f'{cool}{aa}') > 4096:
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'auth-key': 'appxapi',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9'
+    }
+    raw_text = input1.text
+        # Extracting opponent's user ID
+    user_id = input1.from_user.id
+    print("Opponent's user_id:", user_id)
+    await input1.delete(True)
+    email, password = raw_text.split("*")
+    payload = {"email": email, "password": password}
+    url = f"https://{api_endpoint}/post/userLogin?extra_details=0"
+    print(f"Modified Login Url :- {url}")
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=payload, headers=headers) as response:
+            login = await response.json()
+            print(f"Login Resp:- {login}")
+            login_data = login.get("data", {})
+            userid = login_data.get("userid", '')
+            token = login_data.get("token", '')
+            hdr1 = {
+                'auth-key':'appxapi',
+                'authorization': token,
+                'accept-encoding':'gzip, deflate, br',
+                'accept-language':'en-US,en;q=0.9'
+            }
+            await editable.edit("**login Successful**")
+            res1 = await fetch_data(session, f"https://{api_endpoint}/get/mycourse?userid={userid}", headers=hdr1)
+            bdetail = res1.get("data", [])
             cool = ""
-        cool += aa
-    await m.reply_text(f"Token for future use:\n\n {token}")
-    await editable.edit(f'{"You have these batches :-"}\n\n{FFF}\n\n{cool}')
-    editable1 = await m.reply_text("Now send the Batch ID to Download")
+            FFF = "**BATCH-ID -      BATCH NAME **"
+            for item in bdetail:
+                id = item.get("id")
+                batch = item.get("course_name")
+                aa = f" `{id}`      - **{batch}**\n\n"
+                if len(f'{cool}{aa}') > 4096:
+                    print(aa)
+                    cool = ""
+                cool += aa
+            await editable.edit(f'{"**You have these batches :-**"}\n\n{FFF}\n\n{cool}')
+            editable1 = await m.reply_text("**Now send the Batch ID to Download**")
+            print("user_id:", m.from_user.id)
+            print("AUTH_USERS:", AUTH_USERS)
 
-    input2 = await bot.listen(editable1.chat.id)
-    raw_text2 = input2.text
-    await input2.delete(True)
+            if user_id is not None and user_id not in AUTH_USERS:
+                print("User ID not in AUTH_USERS")
 
-    editable2 = await m.reply_text("📥Please wait patiently. 🧲Scraping Url...")
-    url2 = requests.get(f"https://elearn.crwilladmin.com/api/v5/batch-topic/{raw_text2}?type=class", headers=headers)
-    keydata2 = json.loads(url2.text)
-    b_data = keydata2["data"]["batch_topic"]
-    filename = keydata2["data"]["batch_detail"]["name"]
-    
-    unique_urls = [] 
-    for topic in b_data:
-        topicName = topic["topicName"]
-        # T_ID = "&".join([topic["id"]])
-        T_ID = "&".join([str(topic["id"])])
-    
-        topic_ids = T_ID.split('&')
-    
-        for u in topic_ids:
-            url3 = requests.get(f"https://elearn.crwilladmin.com/api/v5/batch-detail/{raw_text2}?redirectBy=mybatch&topicId={u}&pToken=", headers=headers)
-            keydata3 = json.loads(url3.text)
-            razz = keydata3["data"]["class_list"]["classes"]
-            razz.reverse()
-            all_urls = ""
-            try:
-                for data in razz:
-                    razid = data["id"]
-                    lessonName = data["lessonName"].replace("/", "_")
-                    lessonExt = data["lessonExt"]
-                    url3 = requests.get(f"https://elearn.crwilladmin.com/api/v5/class-detail/{razid}", headers=headers)
-                    lessonUrl = json.loads(url3.text)['data']['class_detail']['lessonUrl']
-                    if lessonExt == 'brightcove':
-                        ntr = requests.get(f"https://elearn.crwilladmin.com/api/v5/livestreamToken?type=brightcove&vid={razid}&module=batch", headers=headers)
-                        master = json.loads(ntr.text)
-                        st2mtok = master["data"]["token"]
-                        link = bc_url + lessonUrl + "/master.m3u8?bcov_auth=" + st2mtok
-                    elif lessonExt == 'youtube':
-                        link = "https://www.youtube.com/embed/" + lessonUrl
-                    cc = f"{lessonName}:{link}"
-                    unique_urls.append(cc)  # Append to list to maintain order
-                await editable2.edit(f"🧲**Scraping videos Url**: `{topicName}`")  # Display topic name only
-            except Exception as e:
-                print(str(e))
-
-    try:
-        url4 = requests.get(f"https://elearn.crwilladmin.com/api/v5/batch-notes/{raw_text2}?b_data={raw_text2}", headers=headers)
-        pdfD = json.loads(url4.text)
-        note = pdfD["data"]["notesDetails"]
-        for data in note:
-            name = data["docTitle"]
-            s = data["docUrl"]
-            cc = f"{name}:{s}"
-            unique_urls.append(cc)  # Append to list to maintain order
-    except Exception as e:
-        print(str(e))
-    
-    # Write all unique URLs into one document file
-    with open(f"{filename}.txt", 'w') as f:
-        for url in unique_urls:
-            f.write(url + '\n')
-    
-    await m.reply_document(
-        document=f"{filename}.txt",
-        caption=f"❤️‍🔥**APP Name**: Career Will\n🥀**Batch Name**: `{filename}`"
-)
-                     
-                    
-                
-                
-        
-        
-        
-
-
-bot.run()
+                await m.reply("**PLEASE UPGRADE YOUR PLAN**", quote=True)
+                return
+            else:
+                   # Continue with the rest of your code here
+                input2 = await bot.listen(editable.chat.id)
+                raw_text2 = input2.text
+                bname = next((x['course_name'] for x in bdetail if str(x['id']) == raw_text2), None)
+                await input2.delete(True)
+                await editable.delete()
+                await editable1.delete()
+                editable2 = await m.reply_text("📥**Please wait patiently. I'll take 2-5 minutes only** 🧲    `Scraping Url...`")
+                res2 = await fetch_data(session, f"https://{api_endpoint}/get/allsubjectfrmlivecourseclass?courseid={raw_text2}&start=-1", headers=hdr1)
+                subject = res2.get("data", [])
+                subjID = "&".join([id["subjectid"] for id in subject])
+                print(f'All Subject Id Info: {subjID}')
+                subject_ids = subjID.split('&')
+                all_urls = ""
+                for u in subject_ids:
+                    res3 = await fetch_data(session, f"https://{api_endpoint}/get/alltopicfrmlivecourseclass?courseid={raw_text2}&subjectid={u}&start=-1", headers=hdr1)
+                    topic = res3.get("data", [])
+                    topicids = "&".join([id["topicid"] for id in topic])
+                    print(f"All topic ids Info : {topicids}")
+                    topicid = topicids.split('&')
+                    for t in topicid:
+                        try:
+                            res4 = await fetch_data(session, f"https://{api_endpoint}/get/livecourseclassbycoursesubtopconceptapiv3?courseid={raw_text2}&subjectid={u}&topicid={t}&start=-1&conceptid=", headers=hdr1)
+                            videodata = res4.get("data", [])
+                            for item in videodata:
+                                pdf_link = item.get("pdf_link", "").replace(":", "=").replace("ZmVkY2JhOTg3NjU0MzIxMA", "==").replace("ZmVkY2JhOTg3NjU0MzIxMA", "")
+                                download_link = item.get("download_link", "").replace(":", "=").replace("ZmVkY2JhOTg3NjU0MzIxMA", "==").replace("ZmVkY2JhOTg3NjU0MzIxMA", "")
+                                name = item["Title"]
+                                content_type = item.get("material_type", "")
+                                if content_type == "VIDEO":
+                                    if pdf_link:  # Check if pdf_link exists
+                                        decrypted_pdf_link = decrypt_link(pdf_link)
+                                        decrypted_video_link = decrypt_link(download_link)
+                                        cc0 = f"{name} - Video: {decrypted_video_link}\n{name} - PDF: {decrypted_pdf_link}"
+                                    else:
+                                        decrypted_video_link = decrypt_link(download_link)
+                                        cc0 = f"{name} - Video: {decrypted_video_link}"
+                                elif content_type == "PDF":
+                                    decrypted_pdf_link = decrypt_link(pdf_link)
+                                    cc0 = f"{name} - PDF: {decrypted_pdf_link}"
+                                all_urls += cc0 + '\n'
+                        except asyncio.exceptions.TimeoutError:
+                            print("Timeout error: The server took too long to respond.")
+                            await m.reply(f"Timeout error: The server took too long to respond. Please Use Indian Proxy")
+                if all_urls:
+                    with open(f"{bname}.txt", 'w', encoding='utf-8') as f:
+                        f.write(all_urls)
+                await editable2.edit("Scraping completed successfully!")
+                await editable2.delete(True)
+                await m.reply_document(
+                    document=f"{bname}.txt",
+                    caption=f"✅** TEXT FILE **✅\n📍**APP Name**: Appx\n🔰**Batch Name**: `{bname}`"
+        )
